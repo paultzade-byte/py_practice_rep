1f9ea9155b9dcc6359c689290611102b4936daf5
paultzade-byte <paultzade@gmail.com>
2026-06-27 14:27:06 +0300

    Refactored test_img_data.py

--- vis_mon_system/tests/test_img_data.py
+++ vis_mon_system/tests/test_img_data.py
@@ -3,46 +3,45 @@ import os
 import pytest
 import sqlite3
 import hashlib
+import base64
+from dataclasses import dataclass
 from src.config import Config
 from src.database.db_manager import ImageRepGetData
 
+# the structure of context
+@dataclass
+class ImgTestContext:
+	db: ImageRepGetData
+	file_name: str
+	img_uuid: str
+	local_binary_img_data: bytes
 
 @pytest.fixture(scope="module")
-def test_manager():
-	# open the DB and take the scrapped data
-	db_manager = ImageRepGetData(Config.DB_PATH)
-	# should be taken from db as last in 'count' 
-	file_name = 'bolt-shirt-1200x1500.c2599ac5f0a35ed5931e.jpg'
+def ctx():
+	db = ImageRepGetData(Config.DB_PATH) 	# open the DB and take the scrapped data
+	file_name = 'bolt-shirt-1200x1500.c2599ac5f0a35ed5931e.jpg' # img name in the DB
 	img_uuid = 'e82c5f41-2f08-4233-a4e5-3beaace62f48'
-	return db_manager, file_name, img_uuid
 
-def test_blob(test_manager):
-	# unpacking the return fron the fixture
-	db_manager, file_name, img_uuid = test_manager
-	row_fetched  = db_manager.get_blob(file_name, img_uuid)
-	blob_fetched = row_fetched["FILE_DATA"]
-
-	img_path = os.path.join(Config.IMG_DIR, file_name)
+	img_path = os.path.join(Config.IMG_DIR, file_name)	# path to standard img
 	with open(img_path, 'rb') as file:
-		local_blob = file.read()
-		assert blob_fetched == local_blob
-
-def test_hash(test_manager):
-	# unpacking the return fron the fixture
-	db_manager, file_name, img_uuid = test_manager
-	row_fetched = db_manager.get_hash(file_name, img_uuid)
-	hash_fetched = row_fetched["FILE_HASH"]
-
-	img_path = os.path.join(Config.IMG_DIR, file_name)
-	hasher = hashlib.md5()
-
-	with open(img_path, 'rb') as file:
-		while chunk := file.read(4096):
-			hasher.update(chunk)
+		local_binary_img_data = file.read()
 	
-	local_hash = hasher.hexdigest()
+	yield ImgTestContext(db, file_name, img_uuid, local_binary_img_data) # return object
+	
+	# close the db connection (teardown)
+	db.close()
 
-	assert hash_fetched == local_hash
+# tests section
+def test_blob(ctx):
+	row_fetched  = ctx.db.get_blob(ctx.file_name, ctx.img_uuid)
+	assert row_fetched["FILE_DATA"] == ctx.local_binary_img_data
 
-def test_base64(test_manager):
-	pass
+def test_hash(ctx):
+	row_fetched = ctx.db.get_hash(ctx.file_name, ctx.img_uuid)
+	local_hash = hashlib.md5(ctx.local_binary_img_data).hexdigest()
+	assert row_fetched["FILE_HASH"] == local_hash
+
+def test_base64(ctx):
+	row_fetched = ctx.db.get_base64(ctx.file_name, ctx.img_uuid)
+	local_base64 = base64.b64encode(ctx.local_binary_img_data).decode('utf-8')
+	assert row_fetched["FILE_B64_DATA"] == local_base64
